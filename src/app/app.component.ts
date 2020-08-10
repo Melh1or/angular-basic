a0193db52e1283689906ba30446060449638b0ff
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-08-10 21:05:05 +0300

    Pass props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  posts: Post[] = [
+    { title: 'Learn Angular', text: 'Some text', id: 1 },
+    { title: 'Learn RxJS', text: 'Some text', id: 2 },
+  ];
+  // titleValue = '';
+  // textValue = '';
+  // addPost = () => {
+  //   const component = {
+  //     title: this.titleValue,
+  //     text: this.textValue
+  //   };
+  //   this.state.push(component);
+  //   this.titleValue = '';
+  //   this.textValue = '';
+  // }
 }
