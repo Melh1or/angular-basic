27fb7f02f15aa057ea88ea62361e7e7e22de83a0
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-08-12 00:13:09 +0300

    Clear projects. Start learning directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 export interface Post {
   title: string;
@@ -11,14 +11,7 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  posts: Post[] = [
-    { title: 'Learn Angular', text: 'Some text', id: 1 },
-    { title: 'Learn RxJS', text: 'Some text', id: 2 },
-  ];
-
-  // tslint:disable-next-line:typedef
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
+export class AppComponent implements OnInit{
+  ngOnInit() {
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostFormComponent,
-    PostComponent,
   ],
   imports: [
     BrowserModule,
