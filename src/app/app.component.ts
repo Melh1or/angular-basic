9db898de8bb58cba5d72c50665b5f238e0653bfa
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-08-12 11:08:37 +0300

    Clear project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 
-export interface Post {
-  title: string;
-  text: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  isVisible = true;
   ngOnInit() {
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {StyleDirective} from './directives/style.directive';
-import { Style2Directive } from './directives/style2.directive';
-import { IfnotDirective } from './directives/ifnot.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDirective,
-    Style2Directive,
-    IfnotDirective
   ],
   imports: [
     BrowserModule,
